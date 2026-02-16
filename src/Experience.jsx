239ab20c4642384e71ec1df9871962b2bce77f90
Helena Oliveira <helena@informach.com.br>
2026-02-16 17:59:10 -0300

    add block spinner

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,3 +1,17 @@
+import { OrbitControls } from '@react-three/drei'
+import { Physics } from '@react-three/rapier'
+import Lights from './Lights.jsx'
+import Level from './Level.jsx'
+
 export default function Experience() {
-    return <></>
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <Physics debug>
+            <Lights />
+
+            <Level />
+        </Physics>
+    </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -11,11 +11,12 @@ root.render(
     <StrictMode>
         <Leva collapsed />
         <Canvas
+            shadows
             camera={{
                 fov: 45,
                 near: 0.1,
                 far: 200,
-                position: [- 4, 3, 6]
+                position: [2.5, 4, 6]
             }}
         >
             <Experience />
