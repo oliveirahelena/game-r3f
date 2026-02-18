d43e186f31e8bbd4cba2b006b1076694a2fde777
Helena Oliveira <helena@informach.com.br>
2026-02-18 11:04:44 -0300

    add follow camera and lights

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,23 +1,33 @@
 import { RigidBody, useRapier } from "@react-three/rapier"
 import { useFrame } from "@react-three/fiber"
 import { useKeyboardControls } from "@react-three/drei"
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
+import * as THREE from "three"
 
 export default function Player() {
     const body = useRef()
     const [subscribeKeys, getKeys] = useKeyboardControls()
+    const { rapier, world } = useRapier()
+
+    const [smoothedCameraPosition] = useState(() => new THREE.Vector3(10, 10, 10))
+    const [smoothedCameraTarget] = useState(() => new THREE.Vector3())
+
 
     const jump = () => {
         const origin = body.current.translation()
         origin.y -= 0.31
         const direction = { x: 0, y: -1, z: 0 }
+        const ray = new rapier.Ray(origin, direction)
+        const hit = world.castRay(ray, 10, true)
 
+        if (hit.timeOfImpact < 0.15) {
+            body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+        }
 
-        body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
     }
 
     useEffect(() => {
-        subscribeKeys(
+        const unsubscribeJump = subscribeKeys(
             (state) => state.jump,
             (value) => {
                 if (value) {
@@ -25,9 +35,15 @@ export default function Player() {
                 }
             },
         )
+
+        return () => {
+            unsubscribeJump()
+        }
     }, [])
 
     useFrame((state, delta) => {
+        // Controls
+
         const { forward, backward, leftward, rightward } = getKeys()
 
         const impulse = { x: 0, y: 0, z: 0 }
@@ -55,6 +71,25 @@ export default function Player() {
 
         body.current.applyImpulse(impulse)
         body.current.applyTorqueImpulse(torque)
+
+        // Camera
+
+        const bodyPosition = body.current.translation()
+
+        const cameraPosition = new THREE.Vector3()
+        cameraPosition.copy(bodyPosition)
+        cameraPosition.z += 2.25
+        cameraPosition.y += 0.65
+
+        const cameraTarget = new THREE.Vector3()
+        cameraTarget.copy(bodyPosition)
+        cameraTarget.y += 0.25
+
+        smoothedCameraPosition.lerp(cameraPosition, 5 * delta)
+        smoothedCameraTarget.lerp(cameraTarget, 5 * delta)
+
+        state.camera.position.copy(smoothedCameraPosition)
+        state.camera.lookAt(smoothedCameraTarget)
     })
 
     return (
